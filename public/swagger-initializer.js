8999e74ccecce901e5f02441cc054279b23ed3ba
bismaamjad558-star <bismaamjad379@gmail.com>
2026-07-28 00:41:18 -0700

    Use swagger-ui-dist for Vercel

---
+++ public/swagger-initializer.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  window.ui = SwaggerUIBundle({
+    url: "/api-docs.json",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
