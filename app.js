55d74818f8289b65856ea26ce279d0701e983eae
bismaamjad558-star <bismaamjad379@gmail.com>
2026-07-30 12:13:54 -0700

    Fix Swagger UI

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.get("/", (req, res) => {
   res.send("Express Routing Server is Running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export default app;
